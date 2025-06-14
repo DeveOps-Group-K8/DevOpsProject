5cc8f8a960bf82b46d7089dc1ab816e1f9f1f4ea
Ugwute Charles Ogbonna <ogbonnacharles684@gmail.com>
2025-06-14 08:47:42 +0100

    Added comment to routes file

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def create_app():
     import routes # Replace with the actual routes you need
     from routes import main, init_app 
     app.register_blueprint(routes.main)  # Register the main blueprint
-    init_app(app)  
+    app.register_blueprint(routes.init_app)  # Register the init_app blueprint
    # app.register_blueprint(routes.auth)  # Register the auth blueprint
 
     return app
